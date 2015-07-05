da6a7e33fb9222b5dfaaa718146f3a63149c82e9
dxeware <dxeware@gmail.com>
2015-07-05 14:51:22 -0700

    Added mouse events -- enter/leave, up/down

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    //alert('mouse entered .ryu div');
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+
+  })
+  .mousedown(function() {
+    //console.log('mousedown');
+    // play hadouken sound
+
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate it to the right of the screen
+  })
+  .mouseup(function() {
+    console.log('mouseup');
+    // ryu goes back to his ready position
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
+
+  });
+
+});
+
